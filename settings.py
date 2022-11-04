149bc5d11f7f0d6c18b4918b317d4c8f05c625b8
Polina Rayvich <111997570+Rayvich@users.noreply.github.com>
2022-11-04 14:30:25 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,20 @@
+class AuthEmail:
+    email = 'rayvich93@gmail.com'
+    password = '123456Qq!'
+
+class AuthPhone:
+    phone = '9164563232'
+    password = '123456Qq!'
+
+class AuthLogin:
+    login = 'rtkid_1667510235946'
+    password = '123456Qq'
+
+class InvalidData:
+    login = 'unknownlogin'
+    password = "123543ERr"
+
+class MAXData:
+    login = 'login'
+    password = "parol"
+
